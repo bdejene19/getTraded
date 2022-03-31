2731aa01f6dd6a9c2ce03edadbc482857deb6e5c
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-03-30 18:11:13 -0700

    Added Notfoun.js, mutation.js and querries.js and made a few changes on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import styled from "styled-components";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
+// import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Nav from "./components/Nav";
 
 function App() {
   return (
     <GlobalStyles>
       <BrowserRouter>
+        {/* <Nav /> */}
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
+           <Route exact path="/login" component={Login} />
+          {/*<Route exact path="/signup" component={Signup} /> */}
+          {/* <Route component={NoMatch} /> */}
+
         </Routes>
       </BrowserRouter>
     </GlobalStyles>
---
+++ client/src/utils/querries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      reviews {
+        _id
+        reviewsText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      reviews {
+        _id
+        reviewsText
+        createdAt
+      }
+    }
+  }
+`;
