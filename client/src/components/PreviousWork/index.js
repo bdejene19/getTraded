8541a66e4bfdca2bfe0c8b94434b572c9084b484
Bemnet Dejene <bdejene19@gmail.com>
2022-03-31 23:48:17 -0400

    finished styling and functionality of carousel to display user's previous work photos - however, small but. Choosing carousel dot index changes photo to selected index, however, lags by setInterval timer length

--- client/src/components/PreviousWork/index.js
+++ client/src/components/PreviousWork/index.js
@@ -1,41 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Container } from "../../pages/Home";
 export default function PreviousWork() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
+  const [sliderXValue, setSliderXValue] = useState(-100);
   const imgs = [
     "https://thehustle.co/wp-content/uploads/2017/10/obamalaugh-1024x538.jpg",
     "https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg",
     "https://reallifeglobal.com/wp-content/uploads/2013/11/lol.jpeg",
   ];
 
+  useEffect(() => {
+    const slideShowImgs = document.querySelectorAll(".slideshow-imgs");
+    const slideDots = document.querySelectorAll("li");
+
+    let slider = setInterval(() => {
+      slideShowImgs.forEach((slide, index) => {
+        slide.style.transform = `translateX(${sliderXValue}%)`;
+        slide.style.transition = `ease-in-out 0.5s `;
+        if (sliderXValue / -100 === index) {
+          slideDots[index].className = "active";
+        } else {
+          slideDots[index].className = "inactive";
+        }
+      });
+
+      let convertedIndex = sliderXValue / -100;
+      if (convertedIndex === imgs.length - 1) {
+        setSliderXValue(0);
+      } else {
+        setSliderXValue(sliderXValue - 100);
+        console.log(sliderXValue);
+      }
+    }, 3000);
+
+    return () => {
+      clearInterval(slider);
+    };
+  }, [sliderXValue]);
   return (
-    <Container>
+    <Container id="previousWork-section">
       <h3>Previous work</h3>
 
-      <PreviousWorkWrapper>
+      <PreviousWorkWrapper id="slideshowImgs-wrapper">
         {imgs.map((item, index) => (
           <CarouselImg
             key={index}
             src={item}
-            style={{ width: "100vw" }}
+            className="slideshow-imgs"
           ></CarouselImg>
         ))}
       </PreviousWorkWrapper>
+      <ul className="dots-container">
+        {imgs.map((item, index) =>
+          index === 0 ? (
+            <li
+              className="active"
+              onClick={() => setSliderXValue(index * -100)}
+            ></li>
+          ) : (
+            <li
+              className="inactive"
+              onClick={() => setSliderXValue(index * -100)}
+            ></li>
+          )
+        )}
+      </ul>
     </Container>
   );
 }
 const CarouselImg = styled.img`
-  width: 100vw;
-  max-width: 80vw;
+  width: 50vw;
+  height: 50vh;
 `;
 const PreviousWorkWrapper = styled.article`
   display: flex;
   flex-direction: row;
-  padding: 3em;
-  width: 100vw;
-
+  /* padding: 3em; */
+  box-shadow: 0 3px 3px black;
+  overflow: hidden;
+  width: 50vw;
+  align-self: center;
   h3 {
     width: 80vw;
   }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,4 +16,24 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+
+  & #previousWork-section {
+    .dots-container {
+      display: flex;
+      column-gap: 1em;
+      align-self: center;
+
+      font-size: 2rem;
+      li {
+        cursor: pointer;
+      }
+      .inactive {
+        list-style: circle;
+      }
+
+      .active: {
+        list-style: inherit;
+      }
+    }
+  }
 `;
