84cc3c3c17c78dfc92d5e6e2aa970b1b6ab5352a
Bemnet Dejene <bdejene19@gmail.com>
2022-04-01 11:39:02 -0400

    accidentally wrote queries into mutations file. Merged and configured queries with backend resolvers

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,35 +1,129 @@
-
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
-    query user($username: String!) {
-        user(username: $username) {
-            _id
-            username
-            email
-            reviews {
-                _id
-                reviewsText
-                createdAt
-            }
+  query user($fullName: String!) {
+    user(fullName: $fullName) {
+      _id
+      createdDate
+      fullName
+      email
+      business {
+        _id
+        createdDate
+        name
+        description
+        owner
+        category {
+          _id
+          name
         }
+        experience {
+          _id
+          workType
+          workDescription
+          workImages
+        }
+        avgScore
+        reviews {
+          _id
+          reviewText
+          reviewAuthor
+          reviewScore
+          createdDate
+        }
+      }
     }
+  }
+`;
+
+export const QUERY_BUSINESSES = gql`
+  query getBusinesses {
+    businessEs {
+      _id
+      createdDate
+      name
+      description
+      owner
+      category {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_SINGLE_BUSINESS = gql`
+  query getSingleBusiness($businessId: ID!) {
+    business(businessId: $businessId) {
+      _id
+      createdDate
+      name
+      description
+      owner
+      category {
+        _id
+        name
+      }
+      experience {
+        _id
+        workType
+        workDescription
+        workImages
+      }
+      avgScore
+      reviews {
+        _id
+        reviewText
+        reviewAuthor
+        reviewScore
+        createdDate
+      }
+    }
+  }
 `;
 
 export const QUERY_ME = gql`
-    query me {
-        me {
-            _id
-            username
-            email
-            reviews {
-                _id
-                reviewsText
-                createdAt
-            }
+  query me {
+    me {
+      _id
+      fullName
+      email
+      businesses {
+        _id
+        createdDate
+        name
+        description
+        owner
+        category {
+          _id
+          name
         }
+        experience {
+          _id
+          workType
+          workDescription
+          workImages
+        }
+        avgScore
+        reviews {
+          _id
+          reviewText
+          reviewAuthor
+          reviewScore
+        }
+      }
     }
-  `;
+  }
+`;
 
 /** For search results - handles shortcut to view business by category */
 export const QUERY_BUSINESS_BY_CATEGORY = gql`
@@ -63,5 +157,4 @@ export const QUERY_BUSINESS_BY_ID = gql`
       reviews
     }
   }
-
 `;
