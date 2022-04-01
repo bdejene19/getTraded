16e72eb09dcd43e6a3bcea38dec8400c91fb5272
Bemnet Dejene <bdejene19@gmail.com>
2022-04-01 13:08:39 -0400

    moved seeded data into seeds folder. Fixed pathing of files. Also fixed server package.json script to run seeds (pathing)

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,30 +1,27 @@
 const db = require("../config/connection");
 const { User, Business, Category } = require("../models/index");
 const userSeeds = require("./userSeeds.json");
-const businessSeeds = require("./businessSeeds.json");
+const businessSeeds = require("./businessSeeds");
 const categorySeeds = require("./categorySeeds.json");
 
 db.once("open", async () => {
   try {
-    await Category.deleteMany({});
-    await Business.deleteMany();
-    await User.deleteMany({});
+    await Category.deleteMany({}).catch((err) => err);
+    await User.deleteMany({}).catch((err) => err);
+    await Business.deleteMany({}).catch((err) => err);
 
-    const categories = await Category.insertMany(categorySeeds);
-    const businesses = await Business.insertMany(businessSeeds);
+    const categories = await Category.create(categorySeeds).catch((err) => err);
 
     console.log("Categories seeded");
-
-    // console.log('business seeded');
-
-    await Business.deleteMany({});
     await User.create(userSeeds);
 
     console.log("Users seeded");
 
     //seed businesses but also add created business id to user business's array
     for (let i = 0; i < businessSeeds.length; i++) {
-      const { _id, owner } = await Business.create(businessSeeds[i]);
+      const { _id, owner } = await Business.create(businessSeeds[i]).catch(
+        (err) => err
+      );
       const user = await User.findOneAndUpdate(
         { fullName: owner },
         {
@@ -32,7 +29,7 @@ db.once("open", async () => {
             business: _id,
           },
         }
-      );
+      ).catch((err) => err);
     }
     //add category ids to respective businesses in businessSeeds
     for (let i = 0; i < businessSeeds.length; i++) {
