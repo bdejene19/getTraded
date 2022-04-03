5c45cb18dfe0bd13c3478095836ea5b17bdc02ce
Ernesto Sanchez <ernestosanchez8888@gmail.com>
2022-04-03 18:29:50 -0400

    Frontend work

--- client/src/components/LargeProfileCard/index.js
+++ client/src/components/LargeProfileCard/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 export default function LargeProfileCard(props) {
-  return (
-    <LargeCardWrapper>
-      <h3>{props.businessName}</h3>
-      <h4>{props.businessField}</h4>
-      <h5>{props.fullName}</h5>
-      <div className="about-section">
-        <h4>About Me</h4>
-        <p>{props.about}</p>
-      </div>
-    </LargeCardWrapper>
-  );
+    return (
+        <LargeCardWrapper>
+            <h3>{props.businessName}</h3>
+            <h4>{props.businessField}</h4>
+            <h5>{props.fullName}</h5>
+            <div className='about-section'>
+                <h4>About</h4>
+                <p>{props.about}</p>
+            </div>
+        </LargeCardWrapper>
+    );
 }
 
 const LargeCardWrapper = styled.div`
-  background-color: snow;
-  box-shadow: 0 3px 3px 3px slategrey;
-  border: solid black 1px;
-  border-radius: 3px;
-  height: 100%;
-  padding: 1em;
+    background-color: snow;
+    box-shadow: 0 3px 3px 3px slategrey;
+    border: solid black 1px;
+    border-radius: 3px;
+    height: 100%;
+    padding: 1em;
 `;
--- client/src/pages/BusinessProfile.js
+++ client/src/pages/BusinessProfile.js
@@ -1,49 +1,54 @@
-import React from "react";
-import styled from "styled-components";
-import LargeProfileCard from "../components/LargeProfileCard";
-import PreviousWork from "../components/PreviousWork";
-import { SmallProfileCard } from "../components/SmallProfileCard";
-import { Container } from "./Home";
-import { useQuery } from "@apollo/client";
-import { QUERY_BUSINESS_BY_ID } from "../utils/queries";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import LargeProfileCard from '../components/LargeProfileCard';
+import ReviewCard from '../components/ReviewCard';
+import ExperienceCard from '../components/ExperienceCard';
+import PreviousWork from '../components/PreviousWork';
+import { SmallProfileCard } from '../components/SmallProfileCard';
+import { Container } from './Home';
+import { useQuery } from '@apollo/client';
+import { QUERY_BUSINESS_BY_ID } from '../utils/queries';
+import { useParams } from 'react-router-dom';
 
 export const BusinessProfile = () => {
-  const { businessId } = useParams();
+    const { businessId } = useParams();
 
-  const { data, loading, error } = useQuery(QUERY_BUSINESS_BY_ID, {
-    variables: {
-      businessId: businessId,
-    },
-  });
+    const { data, loading, error } = useQuery(QUERY_BUSINESS_BY_ID, {
+        variables: {
+            businessId: businessId,
+        },
+    });
 
-  const businessData = data?.businessById || null;
-  // console.log(JSON.stringify(error, null, 2));
+    const businessData = data?.businessById || null;
+    // console.log(JSON.stringify(error, null, 2));
 
-  return (
-    <Container>
-      {loading ? (
-        <h1>...loading</h1>
-      ) : (
-        <ProfileInfoWrapper>
-          <div className="smallCards-Container">
-            <SmallProfileCard
-              cardHeader="Average Work Rating"
-              cardContent={businessData.avgScore}
-            />
-            <SmallProfileCard cardHeader="Completed Jobs" cardContent="7" />
-          </div>
-          <div className="largeCard-Container">
-            <LargeProfileCard
-              businessName={businessData.name}
-              fullName={businessData.owner}
-              about={businessData.description}
-              previousWork={businessData.experience}
-            ></LargeProfileCard>
-          </div>
-        </ProfileInfoWrapper>
-      )}
-      {/* <ProfileInfoWrapper>
+    return (
+        <Container>
+            {loading ? (
+                <h1>...loading</h1>
+            ) : (
+                <ProfileInfoWrapper>
+                    <div className='smallCards-Container'>
+                        <SmallProfileCard
+                            cardHeader='Average Work Rating'
+                            cardContent={businessData.avgScore}
+                        />
+                        <SmallProfileCard
+                            cardHeader='Completed Jobs'
+                            cardContent='7'
+                        />
+                    </div>
+                    <div className='largeCard-Container'>
+                        <LargeProfileCard
+                            businessName={businessData.name}
+                            fullName={businessData.owner}
+                            about={businessData.description}
+                            previousWork={businessData.experience}
+                        ></LargeProfileCard>
+                    </div>
+                </ProfileInfoWrapper>
+            )}
+            {/* <ProfileInfoWrapper>
         <div className="smallCards-Container">
           <SmallProfileCard
             cardHeader="Average Work Rating"
@@ -55,29 +60,61 @@ export const BusinessProfile = () => {
           <LargeProfileCard fullName="Bemnet Dejene "></LargeProfileCard>
         </div>
       </ProfileInfoWrapper> */}
-      <PreviousWork></PreviousWork>
-    </Container>
-  );
+            <PreviousWork></PreviousWork>
+            {/* <div className='card'>
+                {businessData.reviews.length == 0 ? (
+                    <h3>No experience posted yet.</h3>
+                ) : (
+                    <ProfileInfoWrapper>
+                        {businessData.experience.map((experience) => (
+                            <ExperienceCard
+                                workType={experience.workType}
+                                workDescription={experience.workDescription}
+                            ></ExperienceCard>
+                        ))}
+                    </ProfileInfoWrapper>
+                )}
+            </div>
+            <div className='row '>
+                <h3 className='container '>Check out Reviews: </h3>
+            </div>
+            <div className='card'>
+                {businessData.reviews.length == 0 ? (
+                    <h3>No reviews.</h3>
+                ) : (
+                    <ProfileInfoWrapper>
+                        {businessData.reviews.map((review) => (
+                            <ReviewCard
+                                reviewName={review.reviewAuthor}
+                                reviewText={review.reviewText}
+                                createdDate={review.createdDate}
+                            ></ReviewCard>
+                        ))}
+                    </ProfileInfoWrapper>
+                )}
+            </div> */}
+        </Container>
+    );
 };
 
 const ProfileInfoWrapper = styled.article`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-
-  .smallCards-Container,
-  .largeCard-Container {
-    padding: 3em;
-  }
-  .smallCards-Container {
-    flex: 1 1 30%;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    row-gap: 2em;
-  }
+    flex-wrap: wrap;
+    width: 100%;
 
-  .largeCard-Container {
-    flex: 1 1 65%;
-  }
+    .smallCards-Container,
+    .largeCard-Container {
+        padding: 3em;
+    }
+    .smallCards-Container {
+        flex: 1 1 30%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 2em;
+    }
+
+    .largeCard-Container {
+        flex: 1 1 65%;
+    }
 `;
